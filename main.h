9a8ac5662bedb2c163c0620877bf8f73286783d2
CODESIMPLEX <echezonaokpala322@gmail.com>
2022-08-09 04:13:18 -0700

    corrections

--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+
 
 /**
  * struct printer - structure for printing various types
@@ -25,11 +25,4 @@ int print_c(va_list c);
 int print_s(va_list s);
 int (*handle_printer_type(const char *format))(va_list);
 
-/* Handlers */
-unsigned char handle_flags(const char *flags, char *index);
-unsigned char handle_length(const char *modifier, char *index);
-int handle_width(va_list args, const char *modifier, char *index);
-int handle_precision(va_list args, const char *modifier, char *index);
-unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
-		unsigned char, int, int, unsigned char);
 #endif /* MAIN_H */
